24f3bdb7eadd9336862209847ccdaea257dddd3f
Siran Gao <sirangao123@gmail.com>
2026-05-01 19:04:56 -0700

    friends page UI: added a friends card

--- frontend/screens/FriendsScreen.js
+++ frontend/screens/FriendsScreen.js
@@ -1,13 +1,27 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import FriendCard from '../components/FriendCard';
+
+const testFriends = [
+    { id: '1', username: 'andrew' },
+    { id: '2', username: 'trevor' },
+    { id: '3', username: 'arnav' },
+    { id: '4', username: 'faris'},
+    { id: '5', username: 'siran'}
+  ];
 
 export default function FriendsScreen() {
   return (
     <View style={styles.container}>
       <Text>Friends</Text>
+      <FlatList
+        data={testFriends}
+        keyExtractor={(item) => item.id}
+        renderItem={({item}) => <FriendCard username={item.username} />}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+  container: { flex: 1},
 });
